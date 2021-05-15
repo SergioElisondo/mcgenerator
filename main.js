1f831cf68e8d6e45ef3bc154e6de2b5b6481a7cd
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-14 20:54:01 -0700

    working chord generator

--- main.js
+++ main.js
@@ -25,10 +25,9 @@ const scribble = require('scribbletune');
 
 // // Get a list of all the available chords
 // const allChords = scribble.chords(); // Returns an array of all the available chords
+// console.log(Object.values(allChords)
 
-// console.group()
-// console.info(allChords)
-// console.groupEnd()
+
 
 // ****************************************** ASCII CODE **********************************
 // think of "x" as quarter note and "-" (aka HYPHEN or Minus) as quarter rest
@@ -39,7 +38,7 @@ const scribble = require('scribbletune');
 
 
 // ******************************** WORD MIXED TO MIX UP CHORDS AND MELODIES **************
-// THIS IS WORD MIXER!! ORIGINAL CODE -- same code below but edited
+// THIS IS THE WORD MIXER!! ORIGINAL CODE -- same code below but edited
 // let tracery = require('tracery-grammar');
  
 // let grammar = tracery.createGrammar({
@@ -53,21 +52,49 @@ const scribble = require('scribbletune');
 // console.log(grammar.flatten('#origin#'));
 
 
+//'note': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
 
 // RANDOMIZER --- EDITED CODE TO WORK FOR RANDOMIZATION
-// let tracery = require('tracery-grammar');
+let tracery = require('tracery-grammar');
  
-// let grammar = tracery.createGrammar({
-//   'chords': ['panda','fox','capybara','iguana'],
-//   'emotion': ['sad','happy','angry','jealous'],
-//   'origin':['I am #emotion.a# #animal#.'],
-// });
- 
-// grammar.addModifiers(tracery.baseEngModifiers); 
- 
-// console.log(grammar.flatten('#origin#'));
+let grammar = tracery.createGrammar({
+    //'definition': ["+add#9", "11th", "11b9", "13th", "13#11", "13#9", "13#9#11", "13b5", "13b9", "13b9#11", "13no5", "13sus4", "4th", "5th", 64, "69#11", "7th", "7#11", "7#11b13", "7#5", "7#5#9", "7#5b9", "7#5b9#11", "7#5sus4", "7#9", "7#9#11", "7#9#11b13", "7#9b13", "7add6", "7b13", "7b5", "7b6", "7b9", "7b9#11", "7b9#9", "7b9b13", "7b9b13#11", "7no5", "7sus4", "7sus4b9", "7sus4b9b13", "9th", "9#11", "9#11b13", "9#5", "9#5#11", "9b13", "9b5", "9no5", "9sus4", "M", "M#5", "M#5add9", "M13", "M13#11", "M6", "M6#11", "M69", "M69#11", "M7#11", "M7#5", "M7#5sus4", "M7#9#11", "M7add13", "M7b5", "M7b6", "M7b9", "M7sus4", "M9", "M9#11", "M9#5", "M9#5sus4", "M9b5", "M9sus4", "Madd9", "Maddb9", "Maj7", "Mb5", "Mb6", "Msus2", "Msus4", "m", "m#5", "m11", "m11A 5", "m11b5", "m13", "m6", "m69", "m7", "m7#5", "m7add11", "m7b5", "m9", "m9#5", "m9b5", "mM9", "mM9b6", "mMaj7", "mMaj7b6", "madd4", "madd9", "mb6M7", "mb6b9", "o", "o7", "o7M7", "oM7", "sus24"],
+    'sadDefinition': ['m', 'm7', 'o', 'o7'],
+    'happyDefinition': ['M', 'Maj7'],
+    'sassyDefinition': ['7add6'],
+    
+    'sadNote': ['A', 'E', 'D'],
+    'happyNote': ['C', 'F', 'G'],
+    'sassyNote': ['B'],
 
+    'chordType': ['#sadNote##sadDefinition#', '#happyNote##happyDefinition#','#sassyNote##sassyDefinition#'],
+    'chord': ['#chordType#'],
+    // 'origin':['#note##chord#'],
+    // 'chords': 
+});
+ 
 
+let getRandomChord = function(grammar){
+        return (grammar.flatten('#chord#'));
+}
+
+let getChords = function(grammar, chordCount){
+    let chords = []
+    for(let i = 0; i < chordCount; i++){
+        let chord = getRandomChord(grammar)
+        console.log('pushing' + chord)
+        chords.push(chord)
+    }
+    return chords
+}
+
+let chordList = getChords(grammar, 8)
+console.log('chordList:', chordList)
+let chords = scribble.clip({
+    notes: chordList,
+    pattern: 'x_x_x_--'.repeat(8)
+})
+scribble.midi(chords, 'random-chords.mid')
 
 
 
@@ -163,13 +190,13 @@ const scribble = require('scribbletune');
 
 
 
-// USING A 7th -- DOMINTANT 7th!
-let userNotes = prompt('give 4 chords: ')
-const cMajorChord = scribble.clip({
-    notes: userNotes,
-    pattern: 'x-'.repeat(4) // gets all 4 chords
-})
-scribble.midi(cMajorChord, 'test.mid')
+// USING A 7th -- DOMINTANT 7th! ******************* WORKING HERE
+// need to add user input
+// const cMajorChord = scribble.clip({
+//     notes: 'Dm7 G7th CM7 CM7',
+//     pattern: 'x-'.repeat(4) // gets all 4 chords
+// })
+// scribble.midi(cMajorChord, 'test.mid')
 
 
 
