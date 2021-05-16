69c72037ec0b174b2831e7bf1a6783e15f7cd786
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-15 20:07:46 -0700

    corrected chord qualities

--- main.js
+++ main.js
@@ -59,15 +59,21 @@ let tracery = require('tracery-grammar');
  
 let grammar = tracery.createGrammar({
     //'definition': ["+add#9", "11th", "11b9", "13th", "13#11", "13#9", "13#9#11", "13b5", "13b9", "13b9#11", "13no5", "13sus4", "4th", "5th", 64, "69#11", "7th", "7#11", "7#11b13", "7#5", "7#5#9", "7#5b9", "7#5b9#11", "7#5sus4", "7#9", "7#9#11", "7#9#11b13", "7#9b13", "7add6", "7b13", "7b5", "7b6", "7b9", "7b9#11", "7b9#9", "7b9b13", "7b9b13#11", "7no5", "7sus4", "7sus4b9", "7sus4b9b13", "9th", "9#11", "9#11b13", "9#5", "9#5#11", "9b13", "9b5", "9no5", "9sus4", "M", "M#5", "M#5add9", "M13", "M13#11", "M6", "M6#11", "M69", "M69#11", "M7#11", "M7#5", "M7#5sus4", "M7#9#11", "M7add13", "M7b5", "M7b6", "M7b9", "M7sus4", "M9", "M9#11", "M9#5", "M9#5sus4", "M9b5", "M9sus4", "Madd9", "Maddb9", "Maj7", "Mb5", "Mb6", "Msus2", "Msus4", "m", "m#5", "m11", "m11A 5", "m11b5", "m13", "m6", "m69", "m7", "m7#5", "m7add11", "m7b5", "m9", "m9#5", "m9b5", "mM9", "mM9b6", "mMaj7", "mMaj7b6", "madd4", "madd9", "mb6M7", "mb6b9", "o", "o7", "o7M7", "oM7", "sus24"],
-    'sadDefinition': ['m', 'm7', 'o', 'o7'],
-    'happyDefinition': ['M', 'Maj7'],
-    'sassyDefinition': [''],
+    'sadDefinition': ['m7'],
+    'happyDefinition': ['Maj7'],
+    'dominantDefinition': ['7th'],
+    'tritoneDefinition': ['7th'],
+    'dimishedDefinition': ['o7'],
+    
     
     'sadNote': ['A', 'E', 'D'],
-    'happyNote': ['C', 'F', 'G'],
-    'sassyNote': ['B'],
+    'happyNote': ['C', 'F'],
+    'dominantNote': ['G'],
+    'tritoneNote': ['Db'], // sub for Dominant7th
+    'diminishedNote': ['B'], // regular diminsished
+    
 
-    'chordType': ['#sadNote##sadDefinition#', '#happyNote##happyDefinition#','#sassyNote##sassyDefinition#'],
+    'chordType': ['#sadNote##sadDefinition#', '#happyNote##happyDefinition#', '#dominantNote##dominantDefinition#','#tritoneNote##tritoneDefinition#', '#diminishedNote##dimishedDefinition#'],
     'chord': ['#chordType#'],
     // 'origin':['#note##chord#'],
     // 'chords': 
@@ -92,7 +98,7 @@ let chordList = getChords(grammar, 8)
 console.log('chordList:', chordList)
 let chords = scribble.clip({
     notes: chordList,
-    pattern: 'x_x_x_--'.repeat(8)
+    pattern: 'x_x_x___'.repeat(8)
 })
 scribble.midi(chords, 'random-chords.mid')
 
