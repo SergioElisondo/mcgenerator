153fb482957e88e329c92f9e41446e476823e581
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-16 16:05:28 -0700

    implemented random rhythm for chords

--- main.js
+++ main.js
@@ -54,15 +54,16 @@ const scribble = require('scribbletune');
 
 //'note': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
 
-// RANDOMIZER --- EDITED CODE TO WORK FOR RANDOMIZATION
+// CHORD MAKER --- EDITED CODE TO WORK FOR RANDOMIZATION
 let tracery = require('tracery-grammar');
  
 let grammar = tracery.createGrammar({
     //'definition': ["+add#9", "11th", "11b9", "13th", "13#11", "13#9", "13#9#11", "13b5", "13b9", "13b9#11", "13no5", "13sus4", "4th", "5th", 64, "69#11", "7th", "7#11", "7#11b13", "7#5", "7#5#9", "7#5b9", "7#5b9#11", "7#5sus4", "7#9", "7#9#11", "7#9#11b13", "7#9b13", "7add6", "7b13", "7b5", "7b6", "7b9", "7b9#11", "7b9#9", "7b9b13", "7b9b13#11", "7no5", "7sus4", "7sus4b9", "7sus4b9b13", "9th", "9#11", "9#11b13", "9#5", "9#5#11", "9b13", "9b5", "9no5", "9sus4", "M", "M#5", "M#5add9", "M13", "M13#11", "M6", "M6#11", "M69", "M69#11", "M7#11", "M7#5", "M7#5sus4", "M7#9#11", "M7add13", "M7b5", "M7b6", "M7b9", "M7sus4", "M9", "M9#11", "M9#5", "M9#5sus4", "M9b5", "M9sus4", "Madd9", "Maddb9", "Maj7", "Mb5", "Mb6", "Msus2", "Msus4", "m", "m#5", "m11", "m11A 5", "m11b5", "m13", "m6", "m69", "m7", "m7#5", "m7add11", "m7b5", "m9", "m9#5", "m9b5", "mM9", "mM9b6", "mMaj7", "mMaj7b6", "madd4", "madd9", "mb6M7", "mb6b9", "o", "o7", "o7M7", "oM7", "sus24"],
     'sadDefinition': ['m7'],
     'happyDefinition': ['Maj7'],
-    'dominantDefinition': ['7th'],
+    'dominantDefinition': ['M'],
     'tritoneDefinition': ['7th'],
+    // 'tritoneDefinitionLow': ['7th-3'],
     'dimishedDefinition': ['o7-3'], // this puts it into a third octave range -- DO NOT put it on chord letters below!!
     
     
@@ -70,8 +71,10 @@ let grammar = tracery.createGrammar({
     'happyNote': ['C', 'F'],
     'dominantNote': ['G'],
     'tritoneNote': ['Db'], // sub for Dominant7th
+    // 'tritoneNoteLow': ['Bb'], // tritone sub for Em7
     'diminishedNote': ['B'], // regular diminsished
     
+    // ADD THIS INTO chordType if you Em7 tritone sub of Bb7 --- '#tritoneNoteLow##tritoneDefinitionLow#',
 
     'chordType': ['#sadNote##sadDefinition#', '#happyNote##happyDefinition#', '#dominantNote##dominantDefinition#','#tritoneNote##tritoneDefinition#', '#diminishedNote##dimishedDefinition#'],
     'text': ['#chordType#'],
@@ -79,6 +82,16 @@ let grammar = tracery.createGrammar({
     // 'chords': 
 });
  
+
+// RHYTHM MAKER 
+let rhythmGrammar = tracery.createGrammar({
+  'groove': ['x__x__x-','x--x__x-','x--x--x_','x-x__x_-'],
+  'text_1':['#groove#'],
+});
+
+
+// MELODY MAKER
+
 
 // --------------------- THIS IS FOR RANDOMIZING CHORDS -------------------------
 let getRandomChord = function(grammar){
@@ -99,13 +112,9 @@ let getChords = function(grammar, chordCount){
 
 
 
-let rhythmGrammar = tracery.createGrammar({
-  'groove': ['x__x__x-','x--x__x-','x--x--x_'],
-  'text':['#groove#'],
-});
 
 let getRandomText = function(grammar) {
-  return grammar.flatten('#text#');
+  return grammar.flatten('#text_1#');
 }
 
 let getText = function(grammar, textCount) {
@@ -128,29 +137,30 @@ console.log('rhythmList:', rhythms);
 // PUTTING IT ALL TOGETHER
 let chords = scribble.clip({
     notes: chordList,
-    pattern: 'x_x_x___'.repeat(8)
+    // pattern: 'x_x_x___'.repeat(8)
 
     // -------------------------  THIS WILL MAKE THE RHYTHM RANDOM!!! -------------------
-    // pattern: rhythms.join('')
+    pattern: rhythms.join('')
 })
 scribble.midi(chords, 'random-chords.mid')
 
 
 
 
-// ************************************* melody ******************* 
-//   const clips = ['1032', '2032', '4021', '3052'].map(order =>
-//     scribble.clip({
-//       pattern: '[xx][xR]'.repeat(4),
-//       notes: scribble.arp({
-//         chords: 'Dm BbM FM CM BbM Am FM Gm',
-//         count: 8,
-//         order,
-//       }),
-//       accent: 'x-xx--xx', // think of "x" as quarter note and "-" as quarter rest
-//     })
-//   );
-//   scribble.midi([].concat(...clips), 'melody.mid');
+// ************************************* MAIN melody ******************* 
+  // const clips = ['1032', '2032', '4021', '3052'].map(order =>
+  //   scribble.clip({
+  //     pattern: '[xx][xR]'.repeat(8),
+  //     notes: scribble.arp({
+  //       // chords: 'Dm BbM FM CM BbM Am FM Gm', //original sounds awesome!
+  //       chords: 'CM Am Em DM GM Am FM GM',
+  //       count: 8,
+  //       order,
+  //     }),
+  //     accent: 'x-xx--xx', // think of "x" as quarter note and "-" as quarter rest
+  //   })
+  // );
+  // scribble.midi([].concat(...clips), 'melody.mid');
 
 
 // ***************************** Arpeggio **********************
