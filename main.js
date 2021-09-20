53da6f39a88af5008e2dbeaec36c387acf4b0606
Sergio Elisondo <sergioelisondo@gmail.com>
2021-09-20 15:09:09 -0700

    implemented new melody gen

--- main.js
+++ main.js
@@ -60,14 +60,15 @@ let grammar = tracery.createGrammar({
   //'chord': ["+add#9", "11th", "11b9", "13th", "13#11", "13#9", "13#9#11", "13b5", "13b9", "13b9#11", "13no5", "13sus4", "4th", "5th", 64, "69#11", "7th", "7#11", "7#11b13", "7#5", "7#5#9", "7#5b9", "7#5b9#11", "7#5sus4", "7#9", "7#9#11", "7#9#11b13", "7#9b13", "7add6", "7b13", "7b5", "7b6", "7b9", "7b9#11", "7b9#9", "7b9b13", "7b9b13#11", "7no5", "7sus4", "7sus4b9", "7sus4b9b13", "9th", "9#11", "9#11b13", "9#5", "9#5#11", "9b13", "9b5", "9no5", "9sus4", "M", "M#5", "M#5add9", "M13", "M13#11", "M6", "M6#11", "M69", "M69#11", "M7#11", "M7#5", "M7#5sus4", "M7#9#11", "M7add13", "M7b5", "M7b6", "M7b9", "M7sus4", "M9", "M9#11", "M9#5", "M9#5sus4", "M9b5", "M9sus4", "Madd9", "Maddb9", "Maj7", "Mb5", "Mb6", "Msus2", "Msus4", "m", "m#5", "m11", "m11A 5", "m11b5", "m13", "m6", "m69", "m7", "m7#5", "m7add11", "m7b5", "m9", "m9#5", "m9b5", "mM9", "mM9b6", "mMaj7", "mMaj7b6", "madd4", "madd9", "mb6M7", "mb6b9", "o", "o7", "o7M7", "oM7", "sus24"],
   sadChord: ["m7-3"],
   happyChord: ["Maj7-3"],
-  dominantChord: ["M-3"],
+  dominantChord: ["M-2"],
   tritoneChord: ["7th-3"],
   // 'tritoneChordLow': ['7th-2'],
   dimishedChord: ["o7-2"], // this puts it into a third octave range -- DO NOT put it on chord letters below
 
   // added G B Eb to sadNotes
   sadNote: ["A", "E", "D"], // G was moved down -- B and Eb was taken out
-  happyNote: ["C", "F", "G"],
+  happyNote: ["C", "F"],
+  happyNote1: ['G'],
 
   // removed 'dominantNote tritoneNote diminishedNote' from the list
   //'dominantNote': ['G'],
@@ -78,7 +79,7 @@ let grammar = tracery.createGrammar({
   // currently not in ORIGINAL set of chords but ADD THIS INTO chordType if you Em7 tritone sub of Bb7 --- '#tritoneNoteLow##tritoneChordLow#',
 
   // THREE ORIGINALS that were removed 060121 add these for chord types if you want dim and tritones in the mix '#dominantNote##dominantChord#','#tritoneNote##tritoneChord#', '#diminishedNote##dimishedChord#'
-  chordType: ["#sadNote##sadChord#", "#happyNote##happyChord#"],
+  chordType: ["#sadNote##sadChord#", "#happyNote##happyChord#", "#happyNote1##dominantChord#"],
   text: ["#chordType#"],
   // 'origin':['#note##chord#'],
   // 'chords':
@@ -102,27 +103,72 @@ let rhythmGrammar = tracery.createGrammar({
 
 
 
+// ****************************************************** NEW MELODY GEN!!!! ************************************
 
-// // Create a clip that plays this C major scale
-// const clip = scribble.clip({
-//   notes: cMajor,
-//   pattern: 'xxxxxxx',
-// });
+// mix it up
+let melodyMix = tracery.createGrammar({
+  notes1: ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'],
+  notesInline: ['#notes1#'],
+}) 
 
-// // mix it up
-// let melodyMix = tracery.createGrammar({
-//   notes1: ['C4', 'D4', 'E4', 'F4']
-//   notesInline: ['#notes1#']
-// })
+// MELODY MAKER!
+let rhythmGrammar1 = tracery.createGrammar({
+  'groove': ['x-xx__x-','-xxx_xx-','--xx-xxx','xxx__x_-'],
+  'text_1': ['#groove#'],
+});
 
-// // MELODY MAKER!
-// let rhythmGrammar = tracery.createGrammar({
-//   'groove': ['x__x__x-','x--x__x-','x--x--x_','x-x__x_-'],
-//   'text_1':['#groove#'],
-// });
+let getRandomNote = function (grammar0) {
+  return grammar0.flatten("#notesInline#");
+};
 
-// // Export this clip as a MIDI file
-// scribble.midi(clip, 'c-major.mid');
+let getNote = function (grammar1, chordCount) {
+  let chords = [];
+  for (let i = 0; i < chordCount; i++) {
+    let chord = getRandomNote(grammar1);
+    console.log("pushing" + chord);
+    chords.push(chord);
+  }
+  return chords;
+};
+
+// seccondary loop for rhythm
+
+let getRandomText1 = function (grammar0) {  // this merges the rhythm aka 'flattens'
+  return grammar0.flatten("#text_1#");
+};
+
+let getText1 = function (grammar2, textCount) {  // anonymous function just for this 
+  let textItems = [];
+  for (let i = 0; i < textCount; i++) {
+    let text = getRandomText1(grammar2); // getRandomText method we created here
+    textItems.push(text);
+  }
+  return textItems;
+};
+
+
+// GRABBIN Notes!
+let chordList1 = getNote(melodyMix, 8); // passing in actual grammar argument from above 
+console.log("chordList1:", chordList1);
+
+// GRABBING RYHTHM
+let rhythm1 = getText1(rhythmGrammar1, 8);
+console.log("rhythm1:", rhythm1);
+
+
+let finalNotes = scribble.clip({
+  notes: chordList1,
+  // pattern: 'x_x_x___'.repeat(8)
+
+  // -------------------------  THIS WILL MAKE THE RHYTHM RANDOM!!! -------------------
+  pattern: rhythm1.join(""),
+});
+
+
+scribble.midi(finalNotes, "melody_gen2.mid");
+
+
+// *************************************************** melody gen end **********************************
 
 
 
@@ -182,27 +228,56 @@ let chords = scribble.clip({
 // THIS OUTPUTS CHORDS ONLY --
 scribble.midi(chords, "chords.mid");
 
+
+
+
+
+
+
+
+
+// Basic Melody Gen from DOCS
 // ************************************* MAIN melody from DOCS *******************
-const clips = ["1032", "2032", "4021", "3052"].map(order =>
-  scribble.clip({
-    pattern: "[xx][xR]".repeat(8),
-    notes: scribble.arp({
-      // chords: 'Dm BbM FM CM BbM Am FM Gm', //original sounds awesome!
-      chords: "CM Am Em Dm GM Am CM GM", // last CM was formally a FM
-      count: 8,
-      order,
-    }),
-    accent: "x-xx--xx", // think of "x" as quarter note and "-" as quarter rest
-  })
-);
+// const clips = ["1032", "2032", "4021", "3052"].map(order =>
+//   scribble.clip({
+//     pattern: "[xx][xR]".repeat(8),
+//     notes: scribble.arp({
+//       // chords: 'Dm BbM FM CM BbM Am FM Gm', //original sounds awesome!
+//       chords: "CM Am Em Dm GM Am CM GM", // last CM was formally a FM
+//       count: 8,
+//       order,
+//     }),
+//     accent: "x-xx--xx", // think of "x" as quarter note and "-" as quarter rest
+//   })
+// );
 
-let melodyNotes = [].concat(...clips); // ... spread operator allowing iterable like an array expression or string to be expanded whereever placed
-// THIS OUTPUTS MELODY ONLY!
-scribble.midi(melodyNotes, "melody.mid");
 
-// this outputs them all together! but not fully working... tried with Leon
-let combined = _.merge(chords, melodyNotes);
-scribble.midi(chords, "AllPartsTogether.mid");
+
+
+
+
+// ******************************************** MERGING ***************************************************************
+
+// let melodyNotes = [].concat(...clips); // ... spread operator allowing iterable like an array expression or string to be expanded whereever placed
+// // THIS OUTPUTS MELODY ONLY!
+// scribble.midi(melodyNotes, "melody.mid");
+
+
+
+
+
+// // this outputs them all together! but not fully working... tried with Leon
+// let combined = _.merge(chords, melodyNotes);
+// scribble.midi(chords, "AllPartsTogether.mid");
+
+
+
+
+
+
+
+
+
 
 // // this outputs them all together! but not fully working...
 // let combined =
