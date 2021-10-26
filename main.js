9a15e3eaba72e461e347b76f515004356b28d556
Sergio Elisondo <sergioelisondo@gmail.com>
2021-10-26 01:04:46 -0700

    changed the rhtyhm on the melo gen

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ let grammar = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar = tracery.createGrammar({
-  groove: ["x__x__x-", "x-xxxxx-", "x--x--x_", "x-x__x_-"],
+  groove: ["x__x__x-", "x-xxxx_-", "x--x--x_", "x-x__x_-"],
   text_1: ["#groove#"],
 });
 
