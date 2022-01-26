73290c631e253f2732b7398d6e262136e9d71fea
Sergio Elisondo <sergioelisondo@gmail.com>
2022-01-26 00:02:06 -0800

    changes rhythm in melo gen

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ let grammar = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar = tracery.createGrammar({
-  groove: ["x__x__x-", "x_xxx__x", "x--x--x_", "x-x__x_-"],
+  groove: ["x__x__x-", "x_xx_x_x", "x--x--x_", "x-x__x_-"],
   text_1: ["#groove#"],
 });
 
