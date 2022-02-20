53f81fb5527bd7e3750a2366a20777352561dfb1
Sergio Elisondo <sergioelisondo@gmail.com>
2022-02-20 02:40:10 -0800

    note 4 extension

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ let grammar = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar = tracery.createGrammar({
-  groove: ["x__x__x-", "xxxx_xxx", "x--x--x_", "x-x__x_-"],
+  groove: ["x__x__x-", "xxx__xxx", "x--x--x_", "x-x__x_-"],
   text_1: ["#groove#"],
 });
 
