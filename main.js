c16b2962d8e597068702eb81794a99bd818ea7a0
Sergio Elisondo <sergioelisondo@gmail.com>
2022-03-10 22:48:39 -0800

    rhythm gen adjusted

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ let grammar = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar = tracery.createGrammar({
-  groove: ["x__x__x-", "xxx_xxx_", "x--x--x_", "x-x__x_-"],
+  groove: ["x__x__x-", "x_x_xxx_", "x--x--x_", "x-x__x_-"],
   text_1: ["#groove#"],
 });
 
