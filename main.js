80b4893a60710ed93eae8cb4af4d5df81302bb8a
Sergio Elisondo <sergioelisondo@gmail.com>
2022-05-03 15:31:22 -0700

    melo gen altered

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ let melodyMix = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar1 = tracery.createGrammar({
-  'groove': ['xx_x_--','xx-xx-xx','-x__x_-x','xxx--__-'],
+  'groove': ['xx_x_--','xx-xx-xx','-x__x_-x','--x-x__x'],
   'text_1': ['#groove#'],
 });
 
